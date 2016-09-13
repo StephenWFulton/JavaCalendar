84e14e76e8d77bfd10c8b57cfd1b39a01aa70636
Adam <adam.van.hal@ku.edu>
2016-09-13 13:07:25 -0500

    Added files for remaining calendar views

---
+++ JavaCalendar/src/calGUI/MonthView.java
@@ -0,0 +1,43 @@
+package calGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MonthView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MonthView window = new MonthView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MonthView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
