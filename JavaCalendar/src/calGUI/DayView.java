c75308bf334004505414df010967ac2022d6d7fe
Adam <adam.van.hal@ku.edu>
2016-09-15 15:06:33 -0500

    Fixed an import in DayView

--- JavaCalendar/src/calGUI/DayView.java
+++ JavaCalendar/src/calGUI/DayView.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import java.awt.BorderLayout;
 import javax.swing.SpringLayout;
 import javax.swing.JTextArea;
 import java.awt.event.MouseAdapter;
