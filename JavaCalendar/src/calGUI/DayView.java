4d64923e995c639463108fe6728665761544a30e
Adam <adam.van.hal@ku.edu>
2016-09-17 15:16:38 -0500

    Added code to fetch dynamic dates

--- JavaCalendar/src/calGUI/DayView.java
+++ JavaCalendar/src/calGUI/DayView.java
@@ -8,10 +8,17 @@ import javax.swing.SpringLayout;
 import javax.swing.JTextArea;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import calClass.DateControl;
+import calIO.CalFile;
+import javax.swing.JTextPane;
+
 
 public class DayView {
-
+	
 	public JFrame frame;
+	
+	private DateControl Date = new DateControl();
 
 	/**
 	 * Launch the application.
@@ -47,7 +54,8 @@ public class DayView {
 		SpringLayout springLayout = new SpringLayout();
 		frame.getContentPane().setLayout(springLayout);
 		
-		JButton btnMonth = new JButton("%Month");
+		JButton btnMonth = new JButton(Date.monthName());
+		springLayout.putConstraint(SpringLayout.NORTH, btnMonth, 10, SpringLayout.NORTH, frame.getContentPane());
 		btnMonth.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
@@ -67,8 +75,6 @@ public class DayView {
 				});
 			}
 		});
-		springLayout.putConstraint(SpringLayout.NORTH, btnMonth, 10, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, btnMonth, 169, SpringLayout.WEST, frame.getContentPane());
 		btnMonth.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
@@ -76,7 +82,7 @@ public class DayView {
 			}
 		});
 		
-		JButton btnweek = new JButton("%Week");
+		JButton btnweek = new JButton("Week: " + Integer.toString(Date.getCurWeek()));
 		btnweek.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
@@ -99,7 +105,7 @@ public class DayView {
 		
 		frame.getContentPane().add(btnMonth);
 		
-		JButton btnYear = new JButton("%Year");
+		JButton btnYear = new JButton(Integer.toString(Date.getCurYear()));
 		btnYear.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
@@ -124,33 +130,37 @@ public class DayView {
 		springLayout.putConstraint(SpringLayout.EAST, btnYear, -149, SpringLayout.EAST, frame.getContentPane());
 		frame.getContentPane().add(btnYear);
 		
-		JTextArea textArea = new JTextArea();
+		final JTextArea textArea = new JTextArea();
+		springLayout.putConstraint(SpringLayout.NORTH, textArea, 85, SpringLayout.NORTH, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, btnYear, -16, SpringLayout.NORTH, textArea);
-		springLayout.putConstraint(SpringLayout.NORTH, textArea, 52, SpringLayout.SOUTH, btnMonth);
 		springLayout.putConstraint(SpringLayout.WEST, textArea, 25, SpringLayout.WEST, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, textArea, 403, SpringLayout.WEST, frame.getContentPane());
 		frame.getContentPane().add(textArea);
 		
 		JButton btnSave = new JButton("Save");
+		springLayout.putConstraint(SpringLayout.SOUTH, textArea, -6, SpringLayout.NORTH, btnSave);
 		btnSave.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				//save current content to text file
-				
+				int curdate = Date.setCurFileDate();
+				String event = textArea.getText();
+				CalFile.CalWrite(curdate, event);
 			}
 		});
-		springLayout.putConstraint(SpringLayout.SOUTH, textArea, -6, SpringLayout.NORTH, btnSave);
 		springLayout.putConstraint(SpringLayout.WEST, btnSave, 185, SpringLayout.WEST, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, btnSave, -10, SpringLayout.SOUTH, frame.getContentPane());
 		frame.getContentPane().add(btnSave);
 		frame.getContentPane().add(btnweek);
 		
 		JButton btnPrevDay = new JButton("< Prev. Day");
+		springLayout.putConstraint(SpringLayout.WEST, btnMonth, 46, SpringLayout.EAST, btnPrevDay);
 		btnPrevDay.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				//set current day - 1
-				
+				int curDay = Date.getCurDay() -1;
+				Date.setCurDay(curDay);
 				//dispose current view
 				frame.dispose();
 				//launch day view
@@ -176,7 +186,8 @@ public class DayView {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				//set current day + 1
-				
+				int curDay = Date.getCurDay() + 1;
+				Date.setCurDay(curDay);
 				//dispose current view
 				frame.dispose();
 				//launch day view
@@ -196,5 +207,11 @@ public class DayView {
 		springLayout.putConstraint(SpringLayout.SOUTH, btnNextDay, 0, SpringLayout.SOUTH, btnPrevDay);
 		springLayout.putConstraint(SpringLayout.EAST, btnNextDay, -10, SpringLayout.EAST, frame.getContentPane());
 		frame.getContentPane().add(btnNextDay);
+		
+		JTextPane txtpnday = new JTextPane();
+		txtpnday.setText(Integer.toString(Date.getCurDay()));
+		springLayout.putConstraint(SpringLayout.WEST, txtpnday, 34, SpringLayout.EAST, btnMonth);
+		springLayout.putConstraint(SpringLayout.SOUTH, txtpnday, 0, SpringLayout.SOUTH, btnMonth);
+		frame.getContentPane().add(txtpnday);
 	}
 }
