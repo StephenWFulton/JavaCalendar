6cf1849e77634d5730f606fc73a28e0955325bd7
Adam <adam.van.hal@ku.edu>
2016-09-17 11:39:41 -0500

    Added event listeners for DayView Buttons

--- JavaCalendar/src/calGUI/DayView.java
+++ JavaCalendar/src/calGUI/DayView.java
@@ -48,6 +48,25 @@ public class DayView {
 		frame.getContentPane().setLayout(springLayout);
 		
 		JButton btnMonth = new JButton("%Month");
+		btnMonth.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				//dispose current view
+				frame.dispose();
+				//launch day view
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							MonthView window = new MonthView();
+							window.frame.setVisible(true);
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
 		springLayout.putConstraint(SpringLayout.NORTH, btnMonth, 10, SpringLayout.NORTH, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, btnMonth, 169, SpringLayout.WEST, frame.getContentPane());
 		btnMonth.addMouseListener(new MouseAdapter() {
@@ -61,7 +80,20 @@ public class DayView {
 		btnweek.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				
+				//dispose current view
+				frame.dispose();
+				//launch day view
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							WeekView window = new WeekView();
+							window.frame.setVisible(true);
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
 			}
 		});
 		
@@ -81,19 +113,68 @@ public class DayView {
 		frame.getContentPane().add(textArea);
 		
 		JButton btnSave = new JButton("Save");
+		btnSave.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				//save current content to text file
+				
+			}
+		});
 		springLayout.putConstraint(SpringLayout.SOUTH, textArea, -6, SpringLayout.NORTH, btnSave);
 		springLayout.putConstraint(SpringLayout.WEST, btnSave, 185, SpringLayout.WEST, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, btnSave, -10, SpringLayout.SOUTH, frame.getContentPane());
 		frame.getContentPane().add(btnSave);
 		frame.getContentPane().add(btnweek);
 		
-		JButton button = new JButton("< Prev. Day");
-		springLayout.putConstraint(SpringLayout.NORTH, button, 21, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, button, 10, SpringLayout.WEST, frame.getContentPane());
-		frame.getContentPane().add(button);
+		JButton btnPrevDay = new JButton("< Prev. Day");
+		btnPrevDay.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				//set current day - 1
+				
+				//dispose current view
+				frame.dispose();
+				//launch day view
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							DayView window = new DayView();
+							window.frame.setVisible(true);
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		springLayout.putConstraint(SpringLayout.NORTH, btnPrevDay, 21, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnPrevDay, 10, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(btnPrevDay);
 		
 		JButton btnNextDay = new JButton("Next Day >");
-		springLayout.putConstraint(SpringLayout.SOUTH, btnNextDay, 0, SpringLayout.SOUTH, button);
+		btnNextDay.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				//set current day + 1
+				
+				//dispose current view
+				frame.dispose();
+				//launch day view
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							DayView window = new DayView();
+							window.frame.setVisible(true);
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		springLayout.putConstraint(SpringLayout.SOUTH, btnNextDay, 0, SpringLayout.SOUTH, btnPrevDay);
 		springLayout.putConstraint(SpringLayout.EAST, btnNextDay, -10, SpringLayout.EAST, frame.getContentPane());
 		frame.getContentPane().add(btnNextDay);
 	}
