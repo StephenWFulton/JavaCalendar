a49b68445d43234bcd959f13a47653e05e00d3ff
Adam <adam.van.hal@ku.edu>
2016-09-17 12:40:34 -0500

    Added Tables for monthview, and the ability to select cells in table.

--- JavaCalendar/src/calGUI/MonthView.java
+++ JavaCalendar/src/calGUI/MonthView.java
@@ -4,12 +4,28 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class MonthView {
 
 	public JFrame frame;
 	private JTable table;
+	private TableModel dataModel = new AbstractTableModel() {
+        public int getColumnCount() { return 7; }
+        public int getRowCount() { return 6;}
+        public Object getValueAt(int row, int col) { 
+        	if(row == 0){
+        		String[] WeekNames = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        		return WeekNames[col];
+        	}
+        	return new Integer(row*col); }
+    };
+
 
 	/**
 	 * Launch the application.
@@ -43,8 +59,18 @@ public class MonthView {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		table = new JTable();
-		table.setBounds(39, 34, 1, 1);
+		table = new JTable(dataModel);
+		table.setCellSelectionEnabled(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);//allow only a single cell to be selected
+		table.setBounds(0, 0, 434, 261);
+		table.setShowGrid(true);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				System.out.println(table.getSelectionModel().getLeadSelectionIndex()); //get selected cell row index
+				System.out.println(table.getColumnModel().getSelectionModel().getLeadSelectionIndex()); //get selected cell column index
+			}
+		});
 		frame.getContentPane().add(table);
 	}
 }
