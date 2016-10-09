62f4211d38a2dee6d263724a6239490c0e4f4d0a
Stephen Fulton <s639f423@ku.edu>
2016-10-09 16:12:27 -0500

    Added remove functionality

--- CalendarProject2/src/calIO/CalFile.java
+++ CalendarProject2/src/calIO/CalFile.java
@@ -154,6 +154,57 @@ public class CalFile{
         return(true);
     }
     
+    public static void remove(String event, int date, boolean removeAll)
+    {
+    	try{
+    		BufferedReader br = new BufferedReader(new FileReader("CalendarInfo.txt"));
+            File file2 = new File("2CalendarInfo.txt");
+            file2.createNewFile();
+            File file = new File("CalendarInfo.txt");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file2));
+            String line = br.readLine();
+            if(removeAll)
+            {
+            	while(line != null)
+            	{
+            		if(line.equals(event)){}
+            		else
+            		{
+            			bw.write(line);
+            			bw.newLine();
+            		}
+            		line = br.readLine();
+            	}
+            }
+            else
+            {
+            	while(!line.equals(date))
+            	{
+            		bw.write(line);
+            		bw.newLine();
+            		line = br.readLine();
+            	}
+            	while(!line.equals(event))
+            	{
+            		bw.write(line);
+            		bw.newLine();
+            		line = br.readLine();
+            	}
+            	line = br.readLine();
+            	while(line != null)
+            	{
+            		bw.write(line);
+            		bw.newLine();
+            		line = br.readLine();
+            	}
+            }
+            
+    	}catch(IOException e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+    
     /*
      *	@pre The CalendarInfo.txt file exists and has proper information and dates in it.
      *	@post None.
